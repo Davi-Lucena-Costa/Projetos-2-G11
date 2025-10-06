e9aff9dece945d70d1e78496aa124fb30e2070ef
BrenoPereira10 <ednaalima80@gmail.com>
2025-10-06 18:39:58 -0300

    Conectando views com html

--- noticias/views.py
+++ noticias/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+# noticias/views.py
+
+from django.shortcuts import render
+from .models import Noticia # Importa o seu modelo Noticia
+
+def home(request):
+    # Busca todas as notícias no banco de dados, ordenando pelas mais recentes
+    ultimas_noticias = Noticia.objects.all().order_by('-data_publicacao')
+    
+    # Define o contexto que será enviado para o HTML
+    contexto = {
+        'lista_de_noticias': ultimas_noticias
+    }
+    
+    # Renderiza o arquivo home.html e envia o contexto para ele
+    return render(request, 'home.html', contexto)
