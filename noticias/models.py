2625078f6e0a89fdfcdadc050a8a1d23eb9902d6
Bernalencouto <coutinhoabernardo@gmail.com>
2025-10-07 11:33:41 -0300

    Mudança do models

--- noticias/models.py
+++ noticias/models.py
@@ -1,18 +1,28 @@
-# models.py
-
 from django.db import models
 from django.contrib.auth.models import User
+
+# Modelo para as categorias das notícias
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.nome
 
 class Noticia(models.Model):
     titulo = models.CharField(max_length=200)
     conteudo = models.TextField(help_text="Conteúdo completo da notícia, com HTML, imagens, etc.")
-    autor = models.CharField(max_length=100)
     data_publicacao = models.DateTimeField(auto_now_add=True)
+    
+    autor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.titulo
+    
+    class Meta:
+        ordering = ['-data_publicacao'] # Ordena as notícias da mais nova para a mais antiga
 
-#Modo de leitura simplificado
 class ModoLeitura(models.Model):
     noticia = models.OneToOneField(
         Noticia, 
@@ -26,14 +36,13 @@ class ModoLeitura(models.Model):
     def __str__(self):
         return f"Modo de Leitura para: {self.noticia.titulo}"
 
-
 class LerMaisTarde(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     noticia = models.ForeignKey(Noticia, on_delete=models.CASCADE)
     salvo_em = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        # Garante que um usuário não possa salvar la mesma notícia mais de uma vez
+        # Garante que um usuário não possa salvar a mesma notícia mais de uma vez
         unique_together = ('usuario', 'noticia')  
 
     def __str__(self):
