bcee00ded5e3f0320a9c2b0be2dfdaa3e4776219
Bernalencouto <coutinhoabernardo@gmail.com>
2025-10-07 11:51:50 -0300

    Mudança no admin e migrations

--- noticias/admin.py
+++ noticias/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Noticia, Categoria, ModoLeitura, LerMaisTarde
 
-# Register your models here.
+# Para gerenciar o ModoLeitura dentro da própria Notícia
+class ModoLeituraInline(admin.StackedInline):
+    model = ModoLeitura
+    can_delete = False
+    verbose_name_plural = 'Modo de Leitura Simplificado'
+    fk_name = 'noticia'
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+    search_fields = ('nome',)
+
+@admin.register(Noticia)
+class NoticiaAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'categoria', 'data_publicacao')
+    list_filter = ('data_publicacao', 'autor', 'categoria') 
+    search_fields = ('titulo', 'conteudo', 'autor__username') 
+    inlines = (ModoLeituraInline,)
+
+@admin.register(LerMaisTarde)
+class LerMaisTardeAdmin(admin.ModelAdmin):
+    list_display = ('noticia', 'usuario', 'salvo_em')
+    list_filter = ('usuario', 'salvo_em')
+    search_fields = ('usuario__username', 'noticia__titulo')
