19487b80ebd421b28a8da8ea10bcdf8c621656a1
Bernalencouto <coutinhoabernardo@gmail.com>
2025-10-12 20:48:39 -0300

    Implementação da função ler mais tarde e ajuste HTML

--- noticias/urls.py
+++ noticias/urls.py
@@ -1,10 +1,19 @@
-# noticias/urls.py (URLS DENTRO DE NOTICIAS)
+# noticias/urls.py
 
 from django.urls import path
-from . import views # Importa as views da sua app
+from . import views
 
 urlpatterns = [
-    # Quando a URL for vazia (homepage), chame a função 'home' da views.py
     path('', views.home, name='home'), 
     path('pesquisa/', views.pesquisar_noticias, name='pesquisar_noticias'),
+
+    
+    # URL para ver o detalhe de uma notícia (ex: /noticia/5/)
+    path('noticia/<int:noticia_id>/', views.detalhe_noticia, name='detalhe_noticia'),
+
+    # URL para a AÇÃO de salvar/remover
+    path('noticia/<int:noticia_id>/salvar/', views.salvar_noticia, name='salvar_noticia'),
+    
+    # URL para a página que lista as notícias salvas
+    path('salvos/', views.lista_salvos, name='lista_salvos'),
 ]
