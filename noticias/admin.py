f7c248533d30624ae29584c2408868f96560f495
BrenoPereira10 <ednaalima80@gmail.com>
2025-10-13 13:24:04 -0300

    Atualizando função pesquisar

--- noticias/admin.py
+++ noticias/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Noticia, Categoria, ModoLeitura, LerMaisTarde
+# IMPORTANTE: Adicione o modelo 'Pesquisa' aqui
+from .models import Noticia, Categoria, ModoLeitura, LerMaisTarde, Pesquisa
 
 # Para gerenciar o ModoLeitura dentro da própria Notícia
 class ModoLeituraInline(admin.StackedInline):
@@ -25,3 +26,21 @@ class LerMaisTardeAdmin(admin.ModelAdmin):
     list_display = ('noticia', 'usuario', 'salvo_em')
     list_filter = ('usuario', 'salvo_em')
     search_fields = ('usuario__username', 'noticia__titulo')
+
+# --- NOVA CLASSE ADMIN ADICIONADA ---
+# Para gerenciar o histórico de Pesquisas no painel de Admin
+@admin.register(Pesquisa)
+class PesquisaAdmin(admin.ModelAdmin):
+    list_display = ('termo_buscado', 'usuario', 'data_busca')
+    list_filter = ('data_busca', 'usuario')
+    search_fields = ('termo_buscado', 'usuario__username')
+    # Torna o painel 'somente leitura', já que não faz sentido editar um histórico de busca
+    readonly_fields = ('termo_buscado', 'usuario', 'data_busca')
+
+    # Desabilita a opção de adicionar novas pesquisas manualmente pelo admin
+    def has_add_permission(self, request):
+        return False
+
+    # Desabilita a opção de deletar pesquisas pelo admin (opcional, mas recomendado)
+    def has_delete_permission(self, request, obj=None):
+        return False
