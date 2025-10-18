4fb578ae0cc70a92639ffcc0e38b25177d38c14c
JOÃO LUCAS DE OLIVEIRA GONÇALVES BAIMA <jhonelucasbay_@outlook.com>
2025-10-18 16:30:40 -0300

    Get&Post + validação com a lógica do models/class SugestaoUser

--- noticias/views.py
+++ noticias/views.py
@@ -5,6 +5,12 @@ from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 # IMPORTANTE: Adicione o modelo 'Pesquisa' aqui
 from .models import Noticia, LerMaisTarde, Pesquisa
+from django.core.exceptions import ValidationError  #Importe para a captura de erros
+from django.contrib import messages  
+from SugestõesUsuarios.models import SugestaoUser # Importe da model/class
+
+
+
 
 def home(request):
     ultimas_noticias = Noticia.objects.all().order_by('-data_publicacao')
@@ -60,3 +66,37 @@ def lista_salvos(request):
     contexto = {'itens_salvos': itens_salvos}
     
     return render(request, 'noticias/salvos.html', contexto)
+
+
+def index(request):
+
+#Dicionário para armazenar o valor anterior e os erros
+    dados_sugestao = {
+        'texto_sugerido': request.POST.get('sugestao_texto', '') , #Pegando o valor anterior
+        'erro': None
+    }
+
+    if request.method == 'POST':
+
+        texto_brut = request.POST.get('sugestao_texto') #Pegamos o valor bruto do post ('sugestao_texto' no html)
+
+        dados_sugestao['texto_sugerido'] = texto_brut  #Mantemos o valor anterior em caso de erro
+
+        try:
+
+            texto_limpo = SugestaoUser.validar_Sugestao(texto_brut) 
+
+            #salvar
+            sugestap = SugestaoUser.objects.create(texto=texto_limpo, usuario=request.user if request.user.is_authenticated else None)
+
+            messages.sucess(request, "Sugestão enviado com sucesso! Obrigado :)")
+            return redirect('noticias:home')  #Redireciona para a home após o sucesso
+        
+        except ValidationError as e:
+
+            dados_sugestao['erro'] = e.message  #Captura a mensagem de erro para exibir no template
+            messages.error(request, f"Erro ao enviar sugestão, verificar conteúdo")
+
+    context = { 'sugestao': dados_sugestao }
+
+    return render(request, 'noticias/sugestao_index.html', context)
