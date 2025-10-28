a7e53f6bdd72d3aa880fa254238ae3849fca3c6c
Rodrigo <rodrigovinhasmarques@gmail.com>
2025-10-28 02:35:28 -0300

    Testes Automatizados

--- noticias/tests.py
+++ noticias/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from .models import Noticia  
 
-# Create your tests here.
+class NoticiaModelTest(TestCase):
+
+    def setUp(self):
+        self.noticia = Noticia.objects.create(
+            titulo="Título de Teste",
+            conteudo="Conteúdo de teste"
+        )
+
+    def test_criacao_noticia(self):
+        self.assertEqual(self.noticia.titulo, "Título de Teste")
+        self.assertEqual(self.noticia.conteudo, "Conteúdo de teste")
