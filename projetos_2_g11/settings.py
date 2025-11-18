a2a369673cefb653aeb460bf3916c9d9abde69c4
BrenoPereira10 <ednaalima80@gmail.com>
2025-11-18 13:38:25 -0300

    correção do settings

--- projetos_2_g11/settings.py
+++ projetos_2_g11/settings.py
@@ -27,7 +27,7 @@ ALLOWED_HOSTS = [
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/home/site/wwwroot/db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  
     }
 }
 
