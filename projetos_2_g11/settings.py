057393b75c4c60507ad00c5a7a907f6526b78ca0
Bernalencouto <coutinhoabernardo@gmail.com>
2025-11-25 12:53:56 -0300

    Correção do workflow e runserver

--- projetos_2_g11/settings.py
+++ projetos_2_g11/settings.py
@@ -5,13 +5,10 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / '.env')
 
-# Ambiente (dev, prod etc.)
 TARGET_ENV = os.getenv('TARGET_ENV', 'dev')
 
-# Ativa modo debug se não for produção
 DEBUG = not TARGET_ENV.lower().startswith('prod')
 
-# Chave secreta 
 SECRET_KEY = os.getenv(
     'SECRET_KEY',
     'django-insecure-g1qu$9_0vz4^!9gdp00eqdztm1p#rf!gpikrc0diz)22pf3!ub'
@@ -23,15 +20,13 @@ ALLOWED_HOSTS = [
     'projetos2g11-bxbscea3eyatdncb.brazilsouth-01.azurewebsites.net'
 ]
 
-# Banco de dados (somente SQLite)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/home/site/wwwroot/db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
-# Apps instalados
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,14 +35,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    # Sua aplicação principal
     'noticias',
-    
-    'whitenoise.runserver_nostatic',
 
+    'whitenoise.runserver_nostatic',
 ]
 
-# Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
@@ -78,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'projetos_2_g11.wsgi.application'
 
-# Validação de senha
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
@@ -94,16 +85,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, 'static')
-]
-
+STATICFILES_DIRS = []
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# No final de projetos_2_g11/settings.py
 
 CSRF_TRUSTED_ORIGINS = [
     "https://projetos2g11-bxbscea3eyatdncb.brazilsouth-01.azurewebsites.net",
