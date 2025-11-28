43923d025af5e78031544d6253fc5054426e0eb2
Davi Costa <dlc@cesar.school>
2025-11-28 20:32:11 -0300

    Desfazer teste com DEBUG e habilitação de arquivos estáticos

--- projetos_2_g11/settings.py
+++ projetos_2_g11/settings.py
@@ -9,7 +9,7 @@ load_dotenv(BASE_DIR / '.env')
 TARGET_ENV = os.getenv('TARGET_ENV', 'dev')
 
 # Ativa modo debug se não for produção
-DEBUG = True
+DEBUG = not TARGET_ENV.lower().startswith('prod')
 
 # Chave secreta 
 SECRET_KEY = os.getenv(
