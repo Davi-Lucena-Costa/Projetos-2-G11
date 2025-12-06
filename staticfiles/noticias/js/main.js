0d0c68e492bf31b97356ee718d4b5ceeccc01ba5
Davi Costa <dlc@cesar.school>
2025-12-05 22:19:18 -0300

    Adicionado função "Edição do dia"

--- staticfiles/noticias/js/main.js
+++ staticfiles/noticias/js/main.js
@@ -26,6 +26,18 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    // --- LÓGICA DO BOTÃO DE EDIÇÃO DO DIA ---
+    
+    const brandBadge = document.querySelector('.brand-badge');
+    if (brandBadge) {
+        brandBadge.addEventListener('click', () => {
+            const url = brandBadge.getAttribute('data-url');
+            if (url) {
+                window.location.href = url;
+            }
+        });
+    }
+
     // --- NOVA LÓGICA DE ACESSIBILIDADE ---
     
     console.log("Carregando controles de acessibilidade...");
